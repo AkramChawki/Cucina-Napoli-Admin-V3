0932281285008585c553b2088797e5a16c046470
AkramChawki <akramchawki01@gmail.com>
2024-07-07 23:41:52 +0100

    fixed admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,11 +58,6 @@ class User extends Authenticatable implements FilamentUser
         return str_ends_with($this->email, '@cucinanapoli.com');
     }
 
-    public function coupons()
-    {
-        return $this->hasMany(Coupon::class);
-    }
-
     public function orders()
     {
         return $this->hasMany(Order::class);
