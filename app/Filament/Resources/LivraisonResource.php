74998aef41342c69d3bb9036ea65233dd6d14d99
akram chawki <btebet@10.0.0.6>
2024-08-09 17:32:51 +0100

    fixed action

--- app/Filament/Resources/LivraisonResource.php
+++ app/Filament/Resources/LivraisonResource.php
@@ -3,16 +3,13 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\LivraisonResource\Pages;
-use App\Filament\Resources\LivraisonResource\RelationManagers;
 use App\Models\Livraison;
-use Filament\Actions\Action;
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Actions\Action;
+
 
 class LivraisonResource extends Resource
 {
