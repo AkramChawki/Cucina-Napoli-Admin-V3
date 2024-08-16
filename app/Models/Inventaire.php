b9e876777cf0777741da695866fd14d0fc2c96f6
AkramChawki <akramchawki01@gmail.com>
2024-08-16 15:36:36 +0100

    blade and model

--- app/Models/Inventaire.php
+++ app/Models/Inventaire.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Inventaire extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'detail' => 'array',
+    ];
+
+    public function products()
+    {
+        return array_map(function ($item) {
+            $p = CuisinierProduct::find($item['product_id']);
+            $p->qty = $item['qty'];
+            return $p;
+        }, $this->detail);
+    }
 }
