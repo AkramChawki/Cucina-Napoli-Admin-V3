b2e2e41ecc4c709801ac3849a716e32a45ffe210
AkramChawki <akramchawki01@gmail.com>
2025-01-03 15:36:11 +0100

    fixed header

--- resources/views/filament/resources/presence/header.blade.php
+++ resources/views/filament/resources/presence/header.blade.php
@@ -1,15 +1,7 @@
 <div class="flex items-center justify-end px-4 py-2">
     <select 
         class="fi-select-input dark:bg-gray-900 dark:text-white dark:border-gray-700 border-gray-300 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-primary-500" 
-        onchange="
-            const currentUrl = new URL(window.location);
-            const tableFilters = currentUrl.searchParams.get('tableFilters[month][value]');
-            let newUrl = '?restaurant=' + this.value;
-            if (tableFilters) {
-                newUrl += '&tableFilters[month][value]=' + tableFilters;
-            }
-            window.location.href = newUrl;
-        "
+        onchange="window.location.search = '?restaurant=' + encodeURIComponent(this.value)"
     >
         <option value="">Tous les restaurants</option>
         @foreach($restaurants as $restaurant)
